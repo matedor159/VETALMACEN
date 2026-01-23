620ddc8e6576bebaa3601f12804c3ba1b819b8c7
HiGuys920 <132838225+HiGuys920@users.noreply.github.com>
2026-01-23 16:10:55 -0500

    Create RequestLoggingMiddlewareExtensions.cs

---
+++ Middlewares/RequestLoggingMiddlewareExtensions.cs
@@ -0,0 +1,7 @@
+public static class RequestLoggingMiddlewareExtensions
+{
+    public static IApplicationBuilder UseRequestLogging(this IApplicationBuilder app)
+    {
+        return app.UseMiddleware<RequestLoggingMiddleware>();
+    }
+}
